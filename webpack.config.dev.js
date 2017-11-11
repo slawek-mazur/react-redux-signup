27f531e58c18839b0bd9dfa707a3b58d71cb50e4
Slawek Mazur <mazursllawek@gmail.com>
2017-11-11 13:47:39 +0100

    - configure hot reload

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,17 +1,27 @@
 import path from 'path';
+import webpack from 'webpack';
 
 export default {
-  entry: path.join(__dirname, '/client/index.js'),
+  entry: [
+    'webpack-hot-middleware/client?reload=true',
+    path.join(__dirname, '/client/index.js'),
+  ],
   output: {
     filename: 'bundle.js',
-    path: '/'
+    path: '/',
+    publicPath: '/'
   },
+  plugins: [
+    new webpack.NoEmitOnErrorsPlugin(),
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin()
+  ],
   module: {
     loaders: [
       {
         test: /\.js$/,
         include: path.join(__dirname, 'client'),
-        loaders: ['babel-loader']
+        loaders: ['babel-loader'] //'react-hot-loader/webpack'
       }
     ]
   },
