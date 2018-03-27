c47fedeaba5b19a271c05d968d018490efa2aa85
mazursllawek@gmail.com <mazursllawek@gmail.com>
2018-03-27 17:33:39 +0200

    - code formatting only

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,25 +1,25 @@
 import express from 'express';
-import validateInput from '../shared/validations/signup';
 import bcrypt from 'bcrypt';
+import validateInput from '../shared/validations/signup';
 
 import User from '../models/user';
 
-let router = express.Router();
+const router = express.Router();
 
 router.post('/', (req, res) => {
   setTimeout(() => {
-    const {errors, isValid} = validateInput(req.body);
+    const { errors, isValid } = validateInput(req.body);
 
     if (!isValid) {
       res.status(400).json(errors);
     } else {
-      const {email, password} = req.body;
+      const { email, password } = req.body;
       let password_digest = bcrypt.hashSync(password, 10);
 
-      User.forge({email, password_digest}, {hasTimestamps: true})
+      User.forge({ email, password_digest: password_digest }, { hasTimestamps: true })
         .save()
-        .then(user => res.json({success: true}))
-        .catch(err => res.status(500).json({error: err}));
+        .then(() => res.json({ success: true }))
+        .catch(err => res.status(500).json(err));
     }
   }, 500);
 });
